03ad1ccc74483436a304c577fed76a65218f08d2
ra100 <git@ra100.net>
2021-03-20 21:36:37 +0100

    👷 Update pipeline

--- cdk/stack.test.ts
+++ cdk/stack.test.ts
@@ -3,7 +3,7 @@ import {expect as expectCDK, matchTemplate, MatchStyle} from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
 import * as Stack from './stack'
 
-test('Empty Stack', () => {
+test('Stack', () => {
   const app = new cdk.App()
   // WHEN
   const stack = new Stack.EcgStack(app, 'MyTestStack')
