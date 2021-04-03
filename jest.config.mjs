e2401733609159810b97cd106adc08f28e901835
ra100 <git@ra100.net>
2021-04-03 09:59:20 +0200

    🔧 Upadte test runs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,7 @@
 export default {
   roots: ['<rootDir>'],
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/cdk.out/', '/build/', '/dist/'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
